5d7f747b32c5b6292169d418e9e719b8713a556b
sherry zheng <sherry.s.y.zheng@noexternalmail.hsbc.com>
2019-07-12 17:34:43 +0800

    add image

--- CoBkzProject/Data/BaseTabBarViewController.m
+++ CoBkzProject/Data/BaseTabBarViewController.m
@@ -15,7 +15,11 @@
 
 @end
 
-@implementation BaseTabBarViewController
+@implementation BaseTabBarViewController{
+    
+    ViewController *vc;
+    TestViewController *v2;
+}
 
 - (void)viewDidLoad {
     
@@ -24,24 +28,21 @@
     [self createTabControllers];
     
     [self createTabbar];
+    
+    self.view.backgroundColor = UIColor.whiteColor;
 }
 
 -(void)createTabControllers{
     
-    ViewController *vc = [[ViewController alloc]init];
+    vc = [[ViewController alloc]init];
     UINavigationController *nav1 = [[UINavigationController alloc]initWithRootViewController:vc];
 
-    TestViewController *v2 = [[TestViewController alloc]init];
+    v2 = [[TestViewController alloc]init];
     UINavigationController *nav2 = [[UINavigationController alloc]initWithRootViewController:v2];
-    
-//    GroupViewController *v3 = [[GroupViewController alloc]init];
-//    UINavigationController *nav3 = [[UINavigationController alloc]initWithRootViewController:v3];
-
     
     NSMutableArray* viewControllers = [[NSMutableArray alloc] init];
     [viewControllers addObject:nav1];
     [viewControllers addObject:nav2];
-//    [viewControllers addObject:nav3];
 
     self.viewControllers = viewControllers;
 }
@@ -50,34 +51,20 @@
     
     UITabBarItem *exeItem = [self.tabBar.items objectAtIndex:0];
     [exeItem setTitle:@"首页"];
-    [exeItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
-    [exeItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
+    [exeItem setImage: [UIImage imageNamed:@"shouye"]];
+    [exeItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:RGB2UIColor(150, 150, 150),NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
+    [exeItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:RGB2UIColor(255, 91, 96),NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
     
     UITabBarItem *testItem = [self.tabBar.items objectAtIndex:1];
     [testItem setTitle:@"测试"];
-    [testItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
-    [testItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
-    
-//    UITabBarItem *groupItem = [self.tabBar.items objectAtIndex:2];
-//    [groupItem setTitle:@"讨论组"];
-//    [groupItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
-//    [groupItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor redColor],NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
+    [testItem setImage: [UIImage imageNamed:@"ceshi"]];
+    [testItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:RGB2UIColor(150, 150, 150),NSForegroundColorAttributeName, nil] forState:UIControlStateNormal];
+    [testItem setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:RGB2UIColor(255, 91, 96),NSForegroundColorAttributeName, nil] forState:UIControlStateSelected];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
